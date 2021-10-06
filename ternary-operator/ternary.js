310853389affe7353c354e07d998acbe17c2034f
Harris Caldwell <harriscaldwell@Harriss-iMac.attlocal.net>
2021-10-05 20:35:21 -0500

    ternary operator

---
+++ ternary-operator/ternary.js
@@ -0,0 +1,12 @@
+let age = 12;
+let status = age >= 18 ? `You are an adult` : `You are a minor`;
+console.log(status)
+
+// ? = if condition is true 
+// : = else
+
+
+let loggedIn = false;
+let profileStatus = loggedIn === true ? `You are logged in` : `You are signed out`;
+console.log(profileStatus)
+
